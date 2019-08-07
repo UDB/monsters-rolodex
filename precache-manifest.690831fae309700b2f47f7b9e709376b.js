840887d561f881e4d1042017c251c7ab497c6fb6
Udit Bhardwaj <itsmeudit@gmail.com>
2019-08-07 16:11:36 +0530

    Updates

---
+++ precache-manifest.690831fae309700b2f47f7b9e709376b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c585cd460bf55dfc3e4302d52f29a05b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21ca764e95ed92b6875e",
+    "url": "/monsters-rolodex/static/css/main.8d1d94d7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "21ca764e95ed92b6875e",
+    "url": "/monsters-rolodex/static/js/main.16b71d90.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
